d971fd867a78f00a29b14d6ef154a84e007f3b22
harold18m <harold.medrano@tecsup.edu.pe>
2024-02-23 19:44:31 -0500

    completed: 99%

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://web-production-2a30.up.railway.app/',
 });
 
 export default api;

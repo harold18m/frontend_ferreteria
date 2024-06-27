561ae99716f3c1b22320017e4635ee7f89830aa0
Harold Medrano <118697566+harold18m@users.noreply.github.com>
2024-06-26 19:04:08 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://cloud.marketingdigital.org.pe:8000/',
+    baseURL: 'http://148.113.174.2:8000/',
 });
 
 export default api;

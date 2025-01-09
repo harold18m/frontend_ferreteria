fa07fd9d21da4f6ab7c7132a14574f7d49072650
harold18m <harold.medrano@tecsup.edu.pe>
2025-01-08 22:10:18 -0500

    feat: actualizar la fuente sans en la configuración de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
+      sans: ['Inter', 'sans-serif'],
     },
     extend: {
       colors: {
